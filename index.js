6d113be59e3a634e7a1e95251683665a781e66c0
Momin Qureshi <mominuhh@gmail.com>
2022-09-24 23:07:03 +0500

    update

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var mongoose = require('mongoose');
-var cors = require('cors');
-var bodyparser = require('body-parser');
-var path = require('path');
+var express = require("express");
+var mongoose = require("mongoose");
+var cors = require("cors");
+var bodyparser = require("body-parser");
+var path = require("path");
 
 var app = express();
 
@@ -10,22 +10,28 @@ app.use(cors());
 app.use(bodyparser.json());
 
 const port = 3000;
-const route = require('./routes/route');
+const route = require("./routes/route");
 
-mongoose.connect('mongodb://localhost:27017/locations',{useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true });
+// mongoose.connect(
+//   "mongodb+srv://mominuhh:<bablue123>@cluster0.7ogwi9c.mongodb.net/?retryWrites=true&w=majority",
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   }
+// );
 
-mongoose.connection.on('connected', ()=>{
-    console.log('mongodb @ 27017');
+// mongoose.connection.on("connected", () => {
+//   console.log("mongodb @ 27017");
+// });
+
+app.use("/api", route);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.send("foobar");
 });
 
-app.use('/api', route);
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res)=>{
-    res.send('foobar');
-});
-
-app.listen(port, ()=>{
-    console.log('server started at ' + port);
+app.listen(port, () => {
+  console.log("server started at " + port);
 });
